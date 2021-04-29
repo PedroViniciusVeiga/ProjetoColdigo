6b82b3b800878a314d4b79bcd5cead6169fde40e
pedro.veiga <pedro_v_veiga@estudante.sc.senai.br>
2021-04-28 22:13:30 -0300

    OT8 - Back-end

---
+++ BDsql/OT1 - Mercado/OT1BD.sql
@@ -0,0 +1,16 @@
+use mercado;
+create table categoria (
+idcategoria int not null primary key auto_increment,
+descricao varchar(45)
+);
+insert into categoria values(1, "Higiene pessoal");
+insert into categoria values(2, "Limpeza");
+insert into categoria values(3, "Cama, mesa e banho");
+insert into categoria values (4, "Frios");
+insert into categoria values (5, "Bazar");
+select * from categoria;
+select descricao from categoria WHERE descricao like 'H%';
+select descricao from categoria WHERE descricao like '%banho';
+select descricao from categoria WHERE descricao like '%es%';
+update categoria set descricao='Frios e laticínios' where idcategoria=4 ;
+delete from categoria where idcategoria=4;
